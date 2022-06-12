6af04c6636f11cfa785a307db6ade270e6f790c3
Pradeep772 <97424653+Pradeep772@users.noreply.github.com>
2022-06-12 21:45:30 +0530

    a=int(input()) arr=[] for i in range(1,a):     if(a%i==0):         arr.append(i)
    if sum(arr)==a:
        print(True)
    else:
        print(False)

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,10 +1,10 @@
-n=int(input())
+a=int(input())
 arr=[]
-for i in  range(1,n):
-    if(n%i==0):
+for i in range(1,a):
+    if(a%i==0):
         arr.append(i)
-b=(sum(arr))
-if(b==n):
+        
+if sum(arr)==a:
     print(True)
 else:
     print(False)
