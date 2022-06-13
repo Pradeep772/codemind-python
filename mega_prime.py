e921c2dadb421e962664c5777c8e077b89130c13
Pradeep772 <97424653+Pradeep772@users.noreply.github.com>
2022-06-13 21:45:09 +0530

    def prime(n):     c=0     for i in range(1,n+1):         if n%i==0:             c+=1     if c==2:         return True     else:         return False n=int(input()) if(prime(n)):     v=0     c=0     while n!=0:         d=n%10         if(prime(d)):             v+=1         n=n//10         c+=1     if v==c:         print("Mega Prime")     else:         print("Not Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,19 +1,25 @@
-n=int(input())
-a=n
-for i in range(2,n//2+1):
-    if(n%i==0):
-        print('Not Mega Prime')
-        break
-else:
-    d=0
-    pd=0
-    while n>0:
-        rem=n%10
-        n=n//10
-        d+=1
-        if rem==3 or rem==2 or rem==5 or rem==7:
-            pd+=1
-    if(d==pd):
-        print('Mega Prime')
+def prime(n):
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c+=1
+    if c==2:
+        return True
     else:
-        print('Not Mega Prime')
+        return False
+n=int(input())
+if(prime(n)):
+    v=0
+    c=0
+    while n!=0:
+        d=n%10
+        if(prime(d)):
+            v+=1
+        n=n//10
+        c+=1
+    if v==c:
+        print("Mega Prime")
+    else:
+        print("Not Mega Prime")
+else:
+    print("Not Mega Prime")
