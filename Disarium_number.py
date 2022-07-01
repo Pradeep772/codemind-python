c2212db2c03ea4f326dbe8eee8506e7fcf0352aa
Pradeep772 <97424653+Pradeep772@users.noreply.github.com>
2022-07-01 18:35:17 +0530

    n=int(input()) s=[int(i) for i in str(n)] a=[] b=0 sum=0 for i in range(len(s)):     b=s[i]**(i+1)     if b!=0:         a.append(b) for i in a:     sum=sum+i if sum==n:     print(True) else:     print(False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,13 +1,19 @@
 n=int(input())
-le=len(str(n))
-temp=n
-s=0
-while n>0:
-    d=n%10
-    s=s+int(d**le)
-    n=n//10
-    le-=1
-if(s==temp):
+s=[int(i) for i in str(n)]
+a=[]
+b=0
+sum=0
+for i in range(len(s)):
+    b=s[i]**(i+1)
+    if b!=0:
+        a.append(b)
+for i in a:
+    sum=sum+i
+if sum==n:
     print(True)
 else:
     print(False)
+    
+    
+    
+    
