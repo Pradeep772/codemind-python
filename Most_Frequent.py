730be605adcb65469d23ead63c741f64f70601ea
Pradeep772 <97424653+Pradeep772@users.noreply.github.com>
2022-07-19 18:59:03 +0530

    n=int(input())
    ls=list(map(int,input().split()))
    
    p=sorted(set(ls))
    
    mx=0
    
    
    
    for i in p:
    
        if ls.count(i)>mx:
    
            mx=ls.count(i)
    
            l=i
    
        if ls.count(i)==mx:
    
            if i<l:
    
                l=i
    
    print(l)

---
+++ Most_Frequent.py
@@ -0,0 +1,25 @@
+n=int(input())
+
+ls=list(map(int,input().split()))
+
+p=sorted(set(ls))
+
+mx=0
+
+
+
+for i in p:
+
+    if ls.count(i)>mx:
+
+        mx=ls.count(i)
+
+        l=i
+
+    if ls.count(i)==mx:
+
+        if i<l:
+
+            l=i
+
+print(l)            
